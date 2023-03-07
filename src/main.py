f82471061b0e919ededbfec328be1c59546626a7
LaurenceMarcotte <laurencemarcotte@hotmail.ca>
2023-03-07 15:53:17 -0500

    Complete profile, add Selector and remove Word and kanji class

--- src/main.py
+++ src/main.py
@@ -1,14 +1,40 @@
-from util import Kanji, Word, Profile
+from util import Profile, Selector
 
-class Selector:
-    pass
 
 if __name__ == "__main__":
-    answer = input("Do you have an existing profile? Y/N")
+    answer = input("Do you have an existing profile? Y/N \n")
     if answer.lower() == "y":
-        username = input("Enter your username: ")
-        userProfile = Profile.load_profile(username)
+        username = input("Enter your username:\n")
+        userProfile = Profile.load(username)
     elif answer.lower() == "n":
-        input("Please enter the username you want to use: ")
-        userProfile = Profile()
+        username = input("Please enter the username you want to use:\n")
+        userProfile = Profile(username)
+    print(userProfile.wordLearned)
+    lesson = userProfile.get_lesson()
+    if lesson == 0:
+        print(f"Welcome {username}, would you like to start learning? Y/N")
+        answer = input("")
+        if answer.lower() == "y":
+            Selector.learn(lesson + 1, userProfile)
+        elif answer.lower() == "n":
+            print("You have not learned any word yet, you cannot revise. Start again when you are ready.")
+    elif lesson > 0:
+        print(f"Welcome back {username}, would you like to: \n (1) learn new words \n (2) revise the words you have seen in previous lessons \n Enter 1 or 2")
+        answer = input("")
+        if answer == "1":
+            Selector.learn(lesson + 1, userProfile)
+        elif answer == "2":
+            Selector.revise(userProfile)
+
+    userProfile.save()
+
+
+    
+
+    # p = pd.read_csv("../media/corpus.csv", delimiter=";", encoding="utf-8")
+    # print(p)
+    # p['kanji']=p['kanji'].str.split(",")
+    # print(p)
+    # p['furigana'] = p['furigana'].str.split(",")
+    # print(p.iloc[1]['kanji'])
 
--- src/util.py
+++ src/util.py
@@ -1,46 +1,22 @@
 import os
 from typing import List, Tuple, Union
-from datetime import date
+from datetime import datetime
 from enum import Enum
+import json
 
-class Kanji:
-    kanji: str
-    furigana: List(str)
+import pandas as pd
 
-    def __init__(self, kanji: str, sound: Union[List[str], str]) -> None:
-        self.kanji = kanji
-        self.furigana = list(sound) if isinstance(sound, str) else sound
+CORPUS = pd.read_csv("../media/corpus.csv", delimiter=";", encoding="utf-8")
 
-    def get_furigana(self, item: int):
-        return self.furigana[item]
+CORPUS["kanji"] = CORPUS["kanji"].str.split(",")
+CORPUS["furigana"] = CORPUS["furigana"].str.split(",")
 
-    def get_idx(self, sound: str):
-        return self.furigana.index(sound)
+KANJI_TO_IDS = CORPUS[CORPUS["word"]==0][["id", "kanji"]]
+KANJI_TO_IDS["kanji"] = KANJI_TO_IDS["kanji"].apply(lambda r: r[0])
+KANJI_TO_IDS = KANJI_TO_IDS.set_index("kanji")
 
-    def get_kanji(self):
-        return self.kanji
+# print(KANJI_TO_IDS)
 
-    def __eq__(self, __o: object) -> bool:
-        return self.kanji == __o
-    
-class Profile:
-    lesson: int = 0
-    username: str
-    kanjiLearned: List[Kanji] = []
-    wordData: List[object] = []
-
-    def __init__(self, username: str) -> None:
-        self.username = username
-
-    def is_kanji_learned(self, kanji):
-        return kanji in self.kanjiLearned
-    
-    @staticmethod
-    def load_profile(username: str, folder: str = "./cache/"):
-        filename = username + ".csv"
-        file = os.path.join(folder, filename)
-        with open(file, "r", encoding="utf-8"):
-            pass
 
 class Boxes(Enum):
     ONE = 1
@@ -51,48 +27,165 @@ class Boxes(Enum):
     SIX = 15
     SEVEN = 25
     EIGHT = 30
+   
+
+class Profile:
+    lesson: int = 0
+    username: str
+    wordLearned: pd.DataFrame
+
+    def __init__(self, username: str, lesson: int = 0, wordLearned: pd.DataFrame = pd.DataFrame()) -> None:
+        self.username = username
+        self.lesson = lesson
+        self.wordLearned = wordLearned
+
+    def is_learned(self, id: int):
+        if len(self.wordLearned) == 0:
+            return False
+        return id in self.wordLearned['id'].to_list()
+    
+    @staticmethod
+    def load(username: str, folder: str = "../cache"):
+        filename = username + ".json"
+        file = os.path.join(folder, filename)
+        with open(file, "r", encoding="utf-8") as f:
+            f = json.load(f)
+        profileCSV = pd.read_csv(f['wordlearned'], encoding="utf-8")
+
+        profileCSV['box']=profileCSV['box'].apply(lambda r: eval(r))
+        
+        return Profile(username, f['lesson'], profileCSV)
+    
+    def save(self, folder: str = "../cache"):
+        filename = self.username + ".json"
+        file = os.path.join(folder, filename)
+        wordLearnedFile = os.path.join(folder, self.username + ".csv")
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        self.wordLearned.to_csv(wordLearnedFile, header=True, index=False)
+        with open(file, "w", encoding = "utf-8") as f:
+            json.dump({"lesson": self.lesson, "wordlearned": wordLearnedFile}, f)
+
+    def new_word_learned(self, newWord: int):
+        # print(self.wordLearned)
+        size = len(self.wordLearned)
+        if size == 0:
+            self.wordLearned = pd.DataFrame([[newWord, datetime.today().strftime("%Y-%m-%d"), Boxes.ONE]], columns=['id', 'lastSeen', 'box'])
+        else:
+            self.wordLearned.loc[size] = [newWord, datetime.today().strftime("%Y-%m-%d"), Boxes.ONE]
+
+    def update_profile(self, newWords: List[int]):
+        for newWord in newWords:
+            self.new_word_learned(newWord)
+
+    def get_lesson(self):
+        return self.lesson
     
 
-class Word:
-    japanese: List[Tuple[Kanji, int]] = []
-    french: str
-    lastSeen: date
-    box: Boxes
+class Selector:
+    
+    def learn(lessonNumber: int, userProfile: Profile):
+        lesson = CORPUS[CORPUS["lesson"]==lessonNumber]
+        kanjis = lesson[lesson["word"]==0]
+        newWords = lesson[lesson["word"]==1]
 
-    def __init__(self, kanjis: List[str], furigana: List[str], kanjiCorpus: List[Kanji], french: str, 
-                 lastSeen: date = None, box: Boxes = Boxes.ONE) -> None:
-
-        if len(kanjis) != len(furigana):
-            raise ValueError("There should be as many kanjis as groups of furiganas.")
-
-        for kanji, f in zip(kanjis, furigana):
-            if kanji in kanjiCorpus:
-                kanji_obj = kanjiCorpus[kanjiCorpus.index(kanji)]
-            else:
-                kanji_obj = Kanji(kanji, f)
-            
-            furi_idx = kanji_obj.get_idx(f)
-            self.japanese.append((kanji_obj, furi_idx))
+        print(f"Let's start with the kanji for lesson {lessonNumber}.")
+        for _, row  in kanjis.iterrows():
+            kanjiID, kanji, furigana, french = row['id'], row['kanji'], row['furigana'], row['french']
+            if not userProfile.is_learned(kanjiID):
+                print("".join(kanji))
+                print("".join(furigana))
+                print(french)
+                enter = input("Press Enter for the next one")
+                userProfile.new_word_learned(kanjiID)
         
-        self.french = french
-        self.lastSeen = lastSeen
-        self.box = box
+        print("Now, let's see some words with these new kanjis.")
+        for _, row in newWords.iterrows():
+            wordID, word, furigana, french = row['id'], row['kanji'], row['furigana'], row['french']
+            kanjiToPrint = ""
+            for kanji, kana in zip(word, furigana):
+                kanjiID = KANJI_TO_IDS.loc[kanji]['id'] if kanji in KANJI_TO_IDS.index else None
+                learned = userProfile.is_learned(kanjiID) if kanjiID is not None else False
+                if learned:
+                    kanjiToPrint += kanji
+                else:
+                    kanjiToPrint += kana
+            print(kanjiToPrint)
+            print("".join(furigana))
+            print(french)
+            enter = input("Press Enter for the next one")
+            userProfile.new_word_learned(wordID)
 
-    def update_score(self):
+        userProfile.lesson = lessonNumber
+
+    
+    def revise(userProfile: Profile):
         pass
 
-    def print(self, lang: str, profile: Profile):
-        word = ""
-        if lang == "jp":
-            for k, idx in self.japanese:
-                if profile.is_kanji_learned(k):
-                    word += k.get_kanji()
-                else:
-                    word += k.get_furigana(idx)
+# class Learnable:
+#     lastSeen: datetime
+#     box: Boxes
+#     french: str
+#     id: int
 
-        elif lang == "fr":
-            word += self.french
 
-        return word
+# class Kanji(Learnable):
+#     kanji: str
+#     furigana: List(str)
+
+#     def __init__(self, kanji: str, sound: Union[List[str], str]) -> None:
+#         self.kanji = kanji
+#         self.furigana = list(sound) if isinstance(sound, str) else sound
+
+#     def get_furigana(self, item: int):
+#         return self.furigana[item]
+
+#     def get_idx(self, sound: str):
+#         return self.furigana.index(sound)
+
+#     def get_kanji(self):
+#         return self.kanji
+
+#     def __eq__(self, __o: object) -> bool:
+#         return self.kanji == __o
+    
+# class Word(Learnable):
+#     japanese: List[Tuple[Kanji, int]] = []
+
+#     def __init__(self, kanjis: List[str], furigana: List[str], kanjiCorpus: List[Kanji], french: str, 
+#                  lastSeen: date = None, box: Boxes = Boxes.ONE) -> None:
+
+#         if len(kanjis) != len(furigana):
+#             raise ValueError("There should be as many kanjis as groups of furiganas.")
+
+#         for kanji, f in zip(kanjis, furigana):
+#             if kanji in kanjiCorpus:
+#                 kanji_obj = kanjiCorpus[kanjiCorpus.index(kanji)]
+#             else:
+#                 kanji_obj = Kanji(kanji, f)
+            
+#             furi_idx = kanji_obj.get_idx(f)
+#             self.japanese.append((kanji_obj, furi_idx))
+        
+#         self.french = french
+#         self.lastSeen = lastSeen
+#         self.box = box
+
+#     def update_score(self):
+#         pass
+
+#     def print(self, lang: str, profile: Profile):
+#         word = ""
+#         if lang == "jp":
+#             for k, idx in self.japanese:
+#                 if profile.is_kanji_learned(k):
+#                     word += k.get_kanji()
+#                 else:
+#                     word += k.get_furigana(idx)
+
+#         elif lang == "fr":
+#             word += self.french
+
+#         return word
 
     
